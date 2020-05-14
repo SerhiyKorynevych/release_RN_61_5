0b321478256b725fc77c551af1d0315960c4243a
serhiykorynevych <serhiy.korynevych@vakoms.com>
2020-05-14 12:56:53 +0300

    first commit

---
+++ android/app/src/main/java/com/rn_61_5_qa/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rn_61_5_qa;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "RN_61_5_QA";
+  }
+}
